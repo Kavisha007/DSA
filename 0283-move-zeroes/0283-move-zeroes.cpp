ae40cbf1bbb455151d157f162d56c38f64840817
Kavisha Mehra <kavishamehra2005@gmail.com>
2026-09-16 22:25:39 +0530

    Time: 419 ms (5.05%), Space: 23.7 MB (82.74%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& arr) {
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-1-i;j++){
+            if(arr[j]==0) swap(arr[j],arr[j+1]);
+            }
+        }
+        
+    }
+};
